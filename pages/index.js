f000b8ef1b8582d016a799e92323ba7214f7dff1
Matías Lescano <mjlescano@protonmail.com>
2017-01-16 10:34:18 -0300

    update images

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ const Intro = () => (
       }
     `}</style>
     <h1 className='tabbed'>
-      Hola! I’m Paz Arando, <br />a ux-ui designer based in <strong>Buenos Aires</strong>.
+      Hola! I’m Paz Arando, <br />a ux-ui designer based in Buenos Aires.
     </h1>
     <h1 className='tabbed'>
       Currently living the startup life at <a href='https://itunes.apple.com/ar/app/tril-things-you-love-from/id988551906' rel='nofollow noopener' target='_blank'>@tril</a>. But also hunting colors<br /> at <a href='https://www.instagram.com/pazarando/' rel='nofollow noopener' target='_blank'>@instagram</a>, and writing about how great<br />  internet is at <a href='https://twitter.com/pazarando' rel='nofollow noopener' target='_blank'>@twitter</a>.
